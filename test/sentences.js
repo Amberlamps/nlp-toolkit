d92c69c24dc9ead892e52659825c5e3e9c83a0d2
Alexander Behrens <alexander.behrens.84@gmail.com>
2016-03-09 22:01:18 +0100

    added tests

---
+++ test/sentences.js
@@ -0,0 +1,23 @@
+/**
+ * Test case for nlp.sentences.
+ */
+
+
+/**
+ * MODULES.
+ */
+var assert = require('assert');
+
+
+/**
+ * TESTS.
+ */
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
